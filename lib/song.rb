76327acffa5672049472c3c59ef508347bc8e33a
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-08-29 02:16:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,8 @@ class Song
     @title = "7/11"
   end 
 
+end 
+
   # song = Song.new("title")
   # I have access to song.artist => instance beyonce
   # I also have access to song.artist.name => "Beyonce "
-  
-end 
